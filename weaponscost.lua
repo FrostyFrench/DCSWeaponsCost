cb07ad28a5609ef4e04b83fbff3d8120093f8322
FrostyFrench <50170138+FrostyFrench@users.noreply.github.com>
2022-05-24 21:45:43 +0100

    Update weaponscost.lua

--- weaponscost.lua
+++ weaponscost.lua
@@ -1,3 +1,5 @@
+local debug_mode = true
+
 Frosty_ShotEvent = {}
 subtotal = 0
 shooting_start = 0
@@ -11,6 +13,7 @@ local weaponcost = {
 ['M_230_new'] = 100
 
 -- ROCKETS --
+['HYDRA_70_M151'] = 2000
 }
 
 function fprint(...)
@@ -20,22 +23,28 @@ function fprint(...)
 	end
 end
 
+function db_fprint(...)
+	if debug_mode == true then
+		fprint(...)
+	end
+end
+
 function Frosty_ShotEvent:onEvent(event)
 	if event.id == 1 then
-		fprint("Weapon Name: " .. event.weapon:getTypeName())
-		fprint("Initiator Coalition: " .. event.initiator:getCoalition())
+		db_fprint("Weapon Name: " .. event.weapon:getTypeName())
 		subtotal = subtotal + weaponcost[event.weapon:getTypeName()]
+		db_fprint("Initiator Coalition: " .. event.initiator:getCoalition())
 	end
 	if event.id == 23 then
 		shooting_start = event.time
-		fprint("shooting_start: " .. shooting_start)
-		fprint("Weapon Name: " .. event.weapon_name)
+		db_fprint("shooting_start: " .. shooting_start)
+		db_fprint("Weapon Name: " .. event.weapon_name)
 	end
 	if event.id == 24 then
 		local time_taken = event.time - shooting_start
-		fprint("Shooting Duration: " .. time_taken)
+		db_fprint("Shooting Duration: " .. time_taken)
 		local rounds_fired = math.modf(time_taken * m230_rof)
-		fprint("Rounds Fired: " .. rounds_fired)
+		db_fprint("Rounds Fired: " .. rounds_fired)
 		subtotal = subtotal + (weaponcost[event.weapon_name] * rounds_fired)
 	end
 end
